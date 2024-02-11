25163a0e271a76ba31ae5c86bdf71ec6ea225c51
Apreel-hub <idrisopeyemi06@gmail.com>
2024-02-11 12:54:54 +0100

    faq 1.0

--- src/screens/faq/Faq.js
+++ src/screens/faq/Faq.js
@@ -7,7 +7,7 @@ import Footer from "../../components/Footer/Footer.js";
 const Faq = () => {
 
     return (
-        <div className="md:mt-12 mt-6">
+        <div className="md:mt-12 mt-6 w-[95%] mx-auto">
             <h1 className="md:text-[3rem] md:text-center text-[1.6rem] text-[#358E07] w-[90%] mx-auto font-black">FAQs</h1>
             <div className="md:grid md:grid-cols-2 md:gap-6 w-[90%] mx-auto mt-4">
                 {
--- src/screens/faq/RenderQuestion.js
+++ src/screens/faq/RenderQuestion.js
@@ -10,16 +10,16 @@ const RenderQuestion = ({question, answer}) => {
     return(
         <div className="">
             <div className="border-b-[.1rem] md:pb-5 pb-3 md:w-[80%] mx-auto">
-                <div className = 'md:w-[100%] mx-auto md:w-[85%] md:mt-8 mt-4'>
+                <div className = 'md:w-[100%] mx-auto md:w-[85%] md:mt-8 mt-6'>
                     <div className="">
-                        <p className ={showAnswer ? 'md:text-[13px] font-semibold text-[12px] text-[#358E07] flex flex-row justify-between' : 'md:text-[13px] font-semibold text-[#454343] text-[12px] flex flex-row justify-between'}>
+                        <p className ={showAnswer ? 'md:text-[13px] font-semibold text-[13px] text-[#358E07] flex flex-row justify-between' : 'md:text-[13px] font-semibold text-[#454343] text-[13px] flex flex-row justify-between'}>
                             {question}
                             <button onClick={() => setShowAnswer(!showAnswer)} className="">
                                 {showAnswer ? <img src="./images/minuss.png" className="w-4"/> : <img src="./images/pluss.png" className="h-4 w-4"/> }
                             </button>               
                         </p>
                     </div>
-                    {showAnswer && <p className= 'md:w-[90%] text-[#454343] md:text-[13px] text-[12px] mt-4 md:leading-[24px] leading-[20px]'>{answer}</p>}
+                    {showAnswer && <p className= 'md:w-[90%] text-[#454343] md:text-[13px] text-[13px] mt-4 md:leading-[24px] leading-[20px]'>{answer}</p>}
                 </div>
             </div>
         </div>
