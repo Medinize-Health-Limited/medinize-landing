07eadc6d7737e1aad7b28aecee034af9de2208e4
Apreel-hub <idrisopeyemi06@gmail.com>
2024-02-11 08:24:33 +0100

    contact 2

--- src/screens/contact/Contact.js
+++ src/screens/contact/Contact.js
@@ -9,21 +9,21 @@ const Contact = () => {
                 <div className="md:flex md:flex-row md:justify-between md:space-x-14 md:mt-16 mt-6 md:w-[93%] w-[87%] mx-auto">
                 <img className="w-[100%] block md:hidden" src="./images/contact.png"/>
                     <div className="w-full md:mt-0 mt-6">
-                        <h1 className="md:text-[2rem] text-[1.7rem] md:font-regular font-bold">Hello,</h1>
-                        <p className="">Send a message, What can we do for you? </p>
+                        <h1 className="md:text-[2rem]text-[#454343]  text-[1.7rem] md:font-regular font-bold">Hello,</h1>
+                        <p className="text-[#454343] ">Send a message, What can we do for you? </p>
                         <Form className="space-y-12 mt-10">
                             <Form.Group className="mb-3 grid space-y-4" controlId="name">
-                                <Form.Label className="label md:text-[13px]">Full Name</Form.Label>
+                                <Form.Label className="text-[#454343] label md:text-[13px]">Full Name</Form.Label>
                                 <input type="text" placeholder=""  required className="outline-none border-b-[0.1rem] border-gray-400"/>
                             </Form.Group>
 
                             <Form.Group className="mb-3 grid space-y-4" controlId="email">
-                                <Form.Label className="label md:text-[13px]">Email Address</Form.Label>
+                                <Form.Label className="text-[#454343] label md:text-[13px]">Email Address</Form.Label>
                                 <input type="text" placeholder=""  required className="outline-none border-b-[0.1rem] border-gray-400"/>
                             </Form.Group>
 
                             <Form.Group className="mb-3 grid space-y-4" controlId="message">
-                                <Form.Label className="label md:text-[13px]">Your message</Form.Label>
+                                <Form.Label className="text-[#454343] label md:text-[13px]">Your message</Form.Label>
                                 <input type="text" placeholder=""  required className="outline-none border-b-[0.1rem] border-gray-400"/>
                             </Form.Group>
                         </Form>
