07ac5ca3a1f44df6b3eb0326a0e0f00b6b2fd0b0
Apreel-hub <idrisopeyemi06@gmail.com>
2024-02-17 09:59:30 +0100

    font-size

--- src/components/Home/Hero/Hero.js
+++ src/components/Home/Hero/Hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
 
 
     return (
-        <div className="mx-[35px]">
+        <div className="mx-[25px]">
             <div className="md:flex md:flex-row md:justify-between md:w-[90%] mx-auto md:mt-12 md:mt-10 mt-6">
                 <div className="md:w-[50%] w-full md:mt-20">
                     <div className="">
@@ -14,7 +14,7 @@ const Hero = () => {
                         Plans with <span className="text-[#358E07]">Medinize</span></h1>
                         <img src="./images/fronds.png" className="md:w-[70%] w-[50%] float-right mr-14"/>
                     </div>
-                    <p className="text-[14px] text-[#454343] mt-6 leading-6 text-justify md:w-[80%]">Our platform enables patients to easily manage health care needs from the comfort of their homes and have easy access to medical services. Unlock the power of medinize and accelerate towards a healthier future.</p>
+                    <p className="text-[14.5px] text-[#454343] mt-6 leading-6 text-justify md:w-[80%]">Our platform enables patients to easily manage health care needs from the comfort of their homes and have easy access to medical services. Unlock the power of medinize and accelerate towards a healthier future.</p>
                     <div className='md:space-x-4 mt-6 md:w-[70%] w-full md:flex md:flex-row md:justify-between md:space-y-0 space-y-3'>
                         <button className="book bg-[#358E07] md:text-[13px] w-full md:w-[50%] text-white py-3 py-[.8rem] px-6 rounded-[5rem]">Book an appointment</button>
                         <button className='access md:bg-[#fff] md:text-[13px] text-[14px] text-[#358E07] md:w-[50%] w-full py-3 px-6 md:rounded-[5rem]'>Access Community</button>
